7395733c3044365634646bf25fea31a803e9b274
Yugendiran <yyugi64@gmail.com>
2022-03-08 23:53:21 +0530

    .

---
+++ db/conn.php
@@ -0,0 +1,17 @@
+<?php
+
+ob_start();
+session_start();
+date_default_timezone_set('Asia/Kolkata');
+
+$current_date = date('d-m-Y H:m:s');
+
+$connection = mysqli_connect('localhost', 'root', '', 'sql');
+
+if(!$connection){
+    echo "<script>alert('Oops... Database Not Connected.');</script>";
+}else{
+    $login_session_user_id = 1;
+}
+
+?>
--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+include "db/conn.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,43 +35,48 @@
 	
 	<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 		<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-			<form class="login100-form validate-form">
+			<form class="login100-form validate-form" action="login.php" method="post">
 				<span class="login100-form-title p-b-37">
 					Sign In
 				</span>
+<?php
+if(isset($_POST['submit'])){
+	$login_email = $_POST['email'];
+	$login_pass = $_POST['pass'];
+
+	$validation_query = "SELECT * FROM users WHERE users_email = '$login_email' AND users_pass = '$login_pass'";
+	$validation_result = mysqli_query($connection, $validation_query);
+
+	if(mysqli_num_rows($validation_result) >= 1){
+		header('location: index.php');
+	}else{
+		?>
+<br>
+<div class="cus_mess red">
+	<p>&#9888; User validation failed. Please try again.</p>
+</div>
+<br>
+		<?php
+	}
+}
+?>
 
 				<div class="wrap-input100 validate-input m-b-20" data-validate="Enter username or email">
-					<input class="input100" type="text" name="username" placeholder="username or email">
+					<input class="input100" type="text" name="email" placeholder="Enter your email">
 					<span class="focus-input100"></span>
 				</div>
 
 				<div class="wrap-input100 validate-input m-b-25" data-validate = "Enter password">
-					<input class="input100" type="password" name="pass" placeholder="password">
+					<input class="input100" type="password" name="pass" placeholder="Enter your password">
 					<span class="focus-input100"></span>
 				</div>
 
 				<div class="container-login100-form-btn">
-					<button class="login100-form-btn">
+					<button class="login100-form-btn" type="submit" name="submit">
 						Sign In
 					</button>
 				</div>
-
-				<div class="text-center p-t-57 p-b-20">
-					<span class="txt1">
-						Or login with
-					</span>
-				</div>
-
-				<div class="flex-c p-b-112">
-					<a href="#" class="login100-social-item">
-						<i class="fa fa-facebook-f"></i>
-					</a>
-
-					<a href="#" class="login100-social-item">
-						<img src="images/icons/icon-google.png" alt="GOOGLE">
-					</a>
-				</div>
-
+	<br>
 				<div class="text-center">
 					<a href="#" class="txt2 hov1">
 						Sign Up
